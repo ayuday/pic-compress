d933630a403c9fbc99ccea6a52b606137aaf6cc6
ayuday <ayuday@163.com>
2024-06-11 15:30:43 +0800

    更新框架配置等文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,39 @@
-import { defineConfig, type ConfigEnv, loadEnv } from 'vite'
+import { defineConfig, type UserConfigExport, type ConfigEnv, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import path from "path";
 
+import {
+  root,
+  alias,
+  wrapperEnv,
+  pathResolve,
+  __APP_INFO__
+} from "./build/utils";
 
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: "/",
-  resolve: {
-    alias: {
-      '@': '/src',
-			'~': '/src/assets'
-      // "@": path.resolve(__dirname, "src"),
+
+
+export default ({ mode }: ConfigEnv): UserConfigExport => {
+  const { VITE_CDN, VITE_PORT, VITE_COMPRESSION, VITE_PUBLIC_PATH } =
+    wrapperEnv(loadEnv(mode, root));
+
+  return {
+    plugins: [vue()],
+    base: "/",
+    resolve: {
+      alias: {
+        '@': '/src',
+        '~': '/src/assets'
+        // "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
-  // 服务端渲染
-  server: {
-    // 端口号
-    port: 3000,
-    host: "0.0.0.0",
-    // 本地跨域代理 https://cn.vitejs.dev/config/server-options.html#server-proxy
-    proxy: {},
-  },
-})
+    // 服务端渲染
+    server: {
+      // 端口号
+      port: 3000,
+      host: "0.0.0.0",
+      // 本地跨域代理 https://cn.vitejs.dev/config/server-options.html#server-proxy
+      proxy: {},
+    }
+  };
+}
